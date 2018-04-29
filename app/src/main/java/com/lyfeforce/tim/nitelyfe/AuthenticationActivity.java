a275797321c346ba6ce06486dcef21b749877a16
theancientmoonstone <timothy.richardson01@gmail.com>
2018-04-30 00:35:35 +1000

    Added AWS SignInUI code Main Activity could not be created due to android studio bug.

--- app/src/main/java/com/lyfeforce/tim/nitelyfe/AuthenticationActivity.java
+++ app/src/main/java/com/lyfeforce/tim/nitelyfe/AuthenticationActivity.java
@@ -3,11 +3,26 @@ package com.lyfeforce.tim.nitelyfe;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+import com.amazonaws.mobile.auth.ui.SignInUI;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.AWSStartupHandler;
+import com.amazonaws.mobile.client.AWSStartupResult;
+
 public class AuthenticationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+
+        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
+            @Override
+            public void onComplete(AWSStartupResult awsStartupResult) {
+                SignInUI signin = (SignInUI) AWSMobileClient.getInstance().getClient(AuthenticationActivity.this, SignInUI.class);
+                signin.login(AuthenticationActivity.this, MainActivity.class).execute();
+            }
+        }).execute();
     }
+
 }
