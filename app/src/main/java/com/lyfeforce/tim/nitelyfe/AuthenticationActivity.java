0a8c83cf4e94be434b925430312e9096d12c5963
Tim <timothy.richardson01@gmail.com>
2018-06-16 19:10:11 +1000

    Added AWS Authentication logic to handle user sign in.

--- app/src/main/java/com/lyfeforce/tim/nitelyfe/AuthenticationActivity.java
+++ app/src/main/java/com/lyfeforce/tim/nitelyfe/AuthenticationActivity.java
@@ -1,7 +1,13 @@
 package com.lyfeforce.tim.nitelyfe;
 
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -10,51 +16,107 @@ import com.amazonaws.mobile.auth.ui.SignInUI;
 import com.amazonaws.mobile.client.AWSMobileClient;
 import com.amazonaws.mobile.client.AWSStartupHandler;
 import com.amazonaws.mobile.client.AWSStartupResult;
+import com.lyfeforce.tim.nitelyfe.AWS.AWSLoginHandler;
+import com.lyfeforce.tim.nitelyfe.AWS.AWSLoginModel;
 
-public class AuthenticationActivity extends AppCompatActivity {
+import static java.sql.DriverManager.println;
 
-    /**
-     * VARIABLE AND OBJECT DECLARATIONS GO BELOW THIS TITLE
-     **/
+public class AuthenticationActivity extends AppCompatActivity implements View.OnClickListener, AWSLoginHandler {
+
+    // AWS Logic
+    AWSLoginModel awsLoginModel;
+
+    // UI References
     EditText UsernameField = (EditText) findViewById(R.id.UsernameField);
     EditText PasswordField = (EditText) findViewById(R.id.PasswordField);
-    private String Username;
-    private String Password;
+    Button SignInButton = (Button) findViewById(R.id.SignInButton);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
 
+        // Prints this line to make sure that everything is working properly.
+        println("Authentication Activity is loading...");
 
+        // Instantiate AWSLoginModel
+        awsLoginModel = new AWSLoginModel(this, this);
+
+        findViewById(R.id.SignInButton).setOnClickListener(this);
+
+/*
         AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
             @Override
             public void onComplete(AWSStartupResult awsStartupResult) {
-                /*
+
                SignInUI signin = (SignInUI) AWSMobileClient.getInstance().getClient(AuthenticationActivity.this, SignInUI.class);
                 signin.login(AuthenticationActivity.this, MainActivity.class).execute();
-                */
+
             }
         }).execute();
+*/
     }
 
-    // Verifies that the user has typed their username into the username field.
-    public void verifyUsernameCompletion() {
-
-        Username = UsernameField.getText().toString();
-        if (Username.matches("")) {
-            Toast.makeText(this,"Please enter your username", Toast.LENGTH_SHORT).show();
-            return;
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            /*
+            case R.id.registerButton:
+            registerAction();
+            break;
+            */
+            /*
+            case R.id.confirmButton:
+            confirmAction();
+            break;
+            */
+            case R.id.SignInButton:
+                loginAction();
+                break;
         }
     }
 
-    // This function will verify that the user has typed their password in the password field.
-    public void verifyPasswordCompletion() {
-        Password = PasswordField.getText().toString();
-        if (Password.matches("")) {
-            Toast.makeText(this,"Please entre your username", Toast.LENGTH_SHORT).show();
-            return;
-        }
+    private void loginAction() {
+        // Handles the user authentication.
+        // Authenticate the user and handle UI flow through the app.
+        awsLoginModel.signInUser(UsernameField.getText().toString(), PasswordField.getText().toString());
+    }
 
+    @Override
+    public void onRegisterSuccess(boolean mustConfirmToComplete) {
+        if (mustConfirmToComplete) {
+            Toast.makeText(AuthenticationActivity.this, "Almost done! Confirm code to complete registration", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(AuthenticationActivity.this, "Registered! Log in!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onRegisterConfirmed() {
+        Toast.makeText(AuthenticationActivity.this, "Registered! Log in!", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onSignInSuccess() {
+        AuthenticationActivity.this.startActivity(new Intent(AuthenticationActivity.this, MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+    }
+
+    @Override
+    public void onFailure(int process, Exception exception) {
+        exception.printStackTrace();
+        String whatProcess = "";
+        switch (process) {
+            case AWSLoginModel.PROCESS_SIGN_IN:
+                whatProcess = "Sign In:";
+                break;
+            case AWSLoginModel.PROCESS_REGISTER:
+                whatProcess = "Register:";
+                break;
+                case AWSLoginModel.PROCESS_CONFIRM_REGISTRATION:
+                    whatProcess = "Registration Confirmation:";
+                    break;
+        }
+        Toast.makeText(AuthenticationActivity.this, whatProcess + exception.getMessage(), Toast.LENGTH_LONG).show();
     }
 }
